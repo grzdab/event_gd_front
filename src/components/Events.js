3420f0abbae37312b9e7464f0ccdad7972781ae2
jakubowskaD <jakubowska.dj@gmail.com>
2022-06-30 13:04:04 +0200

    pagination

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,87 +1,64 @@
-import React, {Component} from 'react';
+import React, {Component, useMemo} from 'react';
 import { useState, useEffect } from 'react';
+import BasicPagination from "./Pagination";
+import Stack from "@mui/material/Stack";
+import Pagination from "@mui/material/Pagination";
+import {Fab} from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+let tableData;
 
 const Events = () => {
 
+    const defaultItem = {
+        "id": "",
+        "property_name": ""
+    }
+
     const [eventsList, setEvents] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [tableData, setItems] = useState([]);
+    const [error, setError] = useState('');
+    const [showDetails, setShowDetails] = useState(false);
+    const [modalHeader, setModalHeader] = useState('Edit equipment');
+    const [modalDescription, setModalDescription] = useState('');
+    const [currentItem, setCurrentItem] = useState(defaultItem);
+    const [currentPage, setCurrentPage] = useState(1);
 
-    // useEffect(() => {
-    //     const getEvents = async() => {
-    //         const data = await fetchEvents();
-    //         setEvents(data);
-    //     }
-    //     getEvents();
-    // }, []);
-    //
-    // const fetchEvents = async () => {
-    //     const response = await fetch('http://localhost:5111/Events');
-    //     const data = await response.json();
-    //     return data;
-    // }
+    const handleCloseDetails = () => {
+        setShowDetails(false);
+        setCurrentItem(defaultItem);
+    };
+    const handleShowDetails = () => setShowDetails(true);
 
-    // hardcoded version
-    const [hardEventsList, setHardEvents] = useState([
-        {  "id": "7edba63-663d-b53d-d2dd-6ecbcb270dc4c",
-            "client": "Company 7edba63-663d",
-            "date": "27-10-2022",
-            "location": "Warsaw",
-            "supervisor": "Salesperson bcb270dc4c" },
-        {  "id": "ee16235-cecd-741c-c224-ad4ec3e31acee",
-            "client": "Company ee16235-cecd",
-            "date": "14-4-2022",
-            "location": "Cracow",
-            "supervisor": "Salesperson ec3e31acee" },
-        {  "id": "2ea20da-dbc4-cec1-c6e0-db0e6cd1dd32d",
-            "client": "Company 2ea20da-dbc4",
-            "date": "7-1-2022",
-            "location": "Madrid",
-            "supervisor": "Salesperson e6cd1dd32d" },
-        {  "id": "c361e6a-26a5-edee-1aa3-0edb5eeb612e1",
-            "client": "Company c361e6a-26a5",
-            "date": "26-3-2022",
-            "location": "New York",
-            "supervisor": "Salesperson b5eeb612e1" },
-        {  "id": "422e08e-c7bb-eb3e-cced-18c731e06c38c",
-            "client": "Company 422e08e-c7bb",
-            "date": "21-6-2022",
-            "location": "Paris",
-            "supervisor": "Salesperson 731e06c38c" },
-        {  "id": "d004c3d-7d1d-78bc-81dc-bd6321e66aa6a",
-            "client": "Company d004c3d-7d1d",
-            "date": "16-10-2022",
-            "location": "London",
-            "supervisor": "Salesperson 321e66aa6a" },
-        {  "id": "4eb2e4c-b4b2-07de-d4d7-71dbc2b52ac20",
-            "client": "Company 4eb2e4c-b4b2",
-            "date": "19-8-2022",
-            "location": "Berlin",
-            "supervisor": "Salesperson bc2b52ac20" },
-        {  "id": "8834ac0-bc7a-7bab-8de0-e6d8b78735ead",
-            "client": "Company 8834ac0-bc7a",
-            "date": "10-12-2022",
-            "location": "Oslo",
-            "supervisor": "Salesperson 8b78735ead" },
-        {  "id": "2db35b2-ea7b-3d6a-e782-ddd3e0463a5c1",
-            "client": "Company 2db35b2-ea7b",
-            "date": "8-1-2022",
-            "location": "Rome",
-            "supervisor": "Salesperson 3e0463a5c1" },
-        {  "id": "70cb5dc-cc7c-78dc-02ee-5bb8da854e73e",
-            "client": "Company 70cb5dc-cc7c",
-            "date": "14-5-2022",
-            "location": "Wdzydze Kiszewskie",
-            "supervisor": "Salesperson 8da854e73e" }
+    useEffect(() => {
+        const getEvents = async() => {
+            const response = await fetch('http://localhost:8080/admin/language');
+            const data = await response.json();
+            if (response.status === 404) {
+                setError('Equipment data not found');
+            }
+            setItems(data);
+            tableData = data;
+            console.log(tableData);
+            // console.log(itemsList);
+            setLoading(false);
+            // console.log(response);
+        }
+        getEvents().catch(console.error)
+    }, []);
 
-    ]);
+    const paginationSize = useMemo(() => {
+        return Math.ceil(tableData.length / 10);
+    });
 
     const fetchById = async (id) => {
-        const response = await fetch(`http://localhost:5111/Events/${id}`);
+        const response = await fetch(`http://localhost:8080/admin/language/${id}`);
         const data = await response.json();
         return data;
     }
 
     const addEvents = async (Events) => {
-        const response = await fetch('http://localhost:5111/Events', {
+        const response = await fetch('http://localhost:8080/admin/language', {
             method: 'POST',
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify(Events)
@@ -92,14 +69,14 @@ const Events = () => {
     }
 
     const deleteEvents = async (id) => {
-        await fetch(`http://localhost:5111/Events/${id}`, {method: 'DELETE',});
+        await fetch(`http://localhost:8080/admin/language/${id}`, {method: 'DELETE',});
         setEvents(eventsList.filter((Events) => Events.id !== id));
     }
 
     const toggleInUse = async (id) => {
         const Events = await fetchById(id);
         const updated = {...Events, inUse: !Events.inUse};
-        const response = await fetch(`http://localhost:5555/Events/${id}`, {
+        const response = await fetch(`http://localhost:8080/admin/language/${id}`, {
             method: 'PUT',
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify(updated)
@@ -112,9 +89,9 @@ const Events = () => {
     }
 
     console.log("EventsList");
-    console.log(eventsList);
+    console.log(tableData);
     console.log("hardEventsList");
-    console.log(hardEventsList);
+    // console.log(hardEventsList);
 
     return (
         <div id="layoutSidenav_content">
@@ -133,25 +110,43 @@ const Events = () => {
                             <table id="datatablesSimple">
                                 <thead>
                                 <tr>
-                                    <th>Client</th>
-                                    <th>Date</th>
-                                    <th>Supervisor</th>
-                                    <th>Location</th>
+                                    <th>id</th>
+                                    <th>Language</th>
                                     <th>Details</th>
                                 </tr>
                                 </thead>
                                 <tbody>
-                                {hardEventsList.map((e) => (
+                                {tableData.map((e) => (
                                     <tr key={e.id}>
-                                        <td>{e.client}</td>
-                                        <td>{e.date}</td>
-                                        <td>{e.supervisor}</td>
-                                        <td>{e.location}</td>
-                                        <td><button className = "btn btn-info" onClick={() => alert("Eq:" + e.name)}>View details</button></td>
+                                        <td>{e.id}</td>
+                                        <td>{e.propertyName}</td>
+                                        <td>
+                                            <Fab
+                                                variant='extended'
+                                                size="small"
+                                            color="inherit"
+                                            aria-label="edit">
+                                            <EditIcon />
+                                        </Fab>
+                                        </td>
+
+                                        {/*<td><button className = "btn btn-info" onClick={() => alert("Eq:" + e.propertyName)}>View details</button></td>*/}
                                     </tr>
                                 ))}
                                 </tbody>
                             </table>
+                            {/*<Stack spacing={2}>*/}
+                                <Pagination
+                                    count={paginationSize}                                    variant="outlined"
+                                    color="primary"
+                                    size="small"
+                                    totalItems={tableData.length}
+                                    itemPerPage={10}
+                                    onChangePage={(pageNumber) => setCurrentPage(pageNumber)}
+                                    boundaryCount={1}
+                                    showFirstButton
+                                    showLastButton />
+                            {/*</Stack>*/}
                         </div>
                     </div>
                 </div>
