3d6d2119f7d79e7752965287dc0bd79ad354a55d
jakubowskaD <jakubowska.dj@gmail.com>
2022-07-01 09:50:22 +0200

    modaL - show with some data (hardcoding)

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,10 +1,12 @@
 import React, {Component, useMemo} from 'react';
 import { useState, useEffect } from 'react';
-import Modal from "./Modal";
+// import Modal from "./Modal";
 import Pagination from "@mui/material/Pagination";
 import {Fab} from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 import ReactPaginate from "react-paginate";
+import Button from "react-bootstrap/Button";
+import {Modal} from "react-bootstrap";
 let tableData;
 let setData;
 
@@ -24,6 +26,7 @@ const Events = () => {
     const [modalDescription, setModalDescription] = useState('');
     const [currentItem, setCurrentItem] = useState(defaultItem);
     const [currentPage, setCurrentPage] = useState(1);
+
 
 
     const handleCloseDetails = () => {
@@ -129,8 +132,14 @@ const Events = () => {
                                                 size="small"
                                             color="inherit"
                                             aria-label="edit">
-                                            <EditIcon />
-<Modal />
+                                            <EditIcon
+                                                onClick={()=>{
+                                                    setModalDescription('Here you can add new equipment. Name and equipment category are required for the application to properly use the equipment. You can add remaining properties at any time.');
+                                                    setModalHeader('Add new equipment');
+                                                    setShowDetails(true);
+                                                }}
+                                            />
+                            {/*<Modal />*/}
                                         </Fab>
                                         </td>
 
@@ -139,7 +148,6 @@ const Events = () => {
                                 ))}
                                 </tbody>
                             </table>
-{/*<Pagination />*/}
                             <Pagination
                                     count={10}
                                     variant="outlined"
@@ -168,9 +176,255 @@ const Events = () => {
                     </div>
                 </div>
             </footer>
+
+    {/*  ============== EQUIPMENT DETAILS MODAL: BEGIN ============== */}
+    <Modal show={showDetails}
+           size="xl"
+           backdrop="static"
+           keyboard={false}
+           onHide={handleCloseDetails}>
+        <Modal.Header className="form-header" closeButton closeVariant="white">
+            <Modal.Title>Equipment details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <section className="mb-4">
+                <h2 className="h1-responsive font-weight-bold text-center my-2">{ modalHeader }</h2>
+                <p className="text-center w-responsive mx-auto mb-5 form_test">{ modalDescription }
+                </p>
+                <div className="row">
+                    <div className="col-md-12 mb-md-0 mb-5">
+                        <form id="add-equipment-form" name="add-equipment-form" action="" method="POST">
+                            <div className="row">
+                                <div className="col-md-8">
+                                    <div className="row">
+                                        <div className="md-form mb-0">
+                                            <label htmlFor="name" className="">Equipment name <span
+                                                className="required">*</span></label>
+                                            <input
+                                                type="text"
+                                                id="name"
+                                                name="name"
+                                                value={tableData.propertyName}
+                                                className="form-control" required
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-8">
+                                            <div className="md-form mb-0">
+                                                <label htmlFor="category" className="">Category <span
+                                                    className="required">*</span></label>
+                                                <select className="form-select"
+                                                        aria-label="Default select example"
+                                                        defaultValue={"DEFAULT"}
+                                                        required
+                                                >
+                                                    <option disabled value="DEFAULT"> -- Select Category -- </option>
+                                                    <option value="1">A</option>
+                                                    <option value="2">B</option>
+                                                    <option value="3">C</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-4">
+                                            <div className="md-form mb-0">
+                                                <label htmlFor="sequence" className="">Sequence<span
+                                                    className="tooltiptext"></span></label>
+                                                <input
+                                                    type="text"
+                                                    id="sequence"
+                                                    name="sequence"
+                                                    value={tableData.id}
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-12">
+                                            <div className="md-form mb-0">
+                                                <label htmlFor="description"
+                                                       className="">Description</label>
+                                                <textarea
+                                                    type="text"
+                                                    id="description"
+                                                    name="description"
+                                                    rows="2"
+                                                    value={currentItem.notes}
+                                                    className="form-control md-textarea"
+                                                ></textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="md-form">
+                                        <label htmlFor="photos">Photos</label>
+                                        <textarea type="text" id="photos" name="photos" rows="2"
+                                                  className="form-control md-textarea"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row margin-top">
+                                <div className="col-md-7">
+                                    <div className="md-form mb-0">
+                                        <div className="card">
+                                            <div className="card-header">
+                                                Technical data
+                                            </div>
+                                            <div className="card-body">
+                                                <div className="row">
+                                                    <div className="col-md-6">
+                                                        <label htmlFor="length" className="">Length
+                                                            (cm)</label>
+                                                        <input
+                                                            type="text"
+                                                            id="length"
+                                                            name="length"
+                                                            value={currentItem.length}
+                                                            className="form-control"
+                                                        />
+                                                        <label htmlFor="width" className="">Width
+                                                            (cm)</label>
+                                                        <input
+                                                            type="text"
+                                                            id="width"
+                                                            name="width"
+                                                            value={currentItem.width}
+                                                            className="form-control"
+                                                        />
+                                                        <label htmlFor="height>" className="">Height
+                                                            (cm)</label>
+                                                        <input
+                                                            type="text"
+                                                            id="height"
+                                                            name="height"
+                                                            value={currentItem.height}
+                                                            className="form-control"
+                                                        />
+                                                    </div>
+                                                    <div className="col-md-6">
+                                                        <label htmlFor="area" className="">Area
+                                                            (m<sup>2</sup>)</label>
+                                                        <input
+                                                            disabled
+                                                            type="text"
+                                                            id="area"
+                                                            name="area"
+                                                            value={currentItem.length * currentItem.width}
+                                                            className="form-control"
+                                                        />
+                                                        <label htmlFor="weight" className="">Weight
+                                                            (kg)</label>
+                                                        <input
+                                                            type="text"
+                                                            id="weight"
+                                                            name="weight"
+                                                            value={currentItem.weight}
+                                                            className="form-control"
+                                                        />
+                                                        <label htmlFor="power>" className="">Power
+                                                            (kW)</label>
+                                                        <input
+                                                            type="text"
+                                                            id="power"
+                                                            name="power"
+                                                            value={currentItem.powerRequired}
+                                                            className="form-control"
+                                                        />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3">
+                                    <div className="card">
+                                        <div className="card-header">
+                                            Conditions
+                                        </div>
+                                        <div className="card-body">
+                                            <label htmlFor="staff" className="">Required staff</label>
+                                            <input
+                                                type="text"
+                                                id="staff"
+                                                name="staff"
+                                                value={currentItem.staffNeeded}
+                                                className="form-control"
+                                            />
+                                            <label htmlFor="minimum_age" className="">Minimum age</label>
+                                            <input
+                                                type="text"
+                                                id="minimum_age"
+                                                name="minimum_age"
+                                                value={currentItem.minimumAge}
+                                                className="form-control"
+                                            />
+                                            <label htmlFor="max_participants>" className="">Max
+                                                participants</label>
+                                            <input
+                                                type="text"
+                                                id="max_participants"
+                                                name="max_participants"
+                                                value={currentItem.maxParticipants}
+                                                className="form-control"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-2">
+                                    <div className="md-form mb-0">
+                                        <div className="card">
+                                            <div className="card-header">
+                                                Status
+                                            </div>
+                                            <div className="card-body">
+                                                <label htmlFor="in_use" className="">In use</label>
+                                                <input
+                                                    type="text"
+                                                    id="in_use"
+                                                    name="in_use"
+                                                    value={currentItem.inUse}
+                                                    className="form-control"
+                                                />
+                                                <label htmlFor="ownership" className="">Ownership</label>
+                                                <input
+                                                    type="text"
+                                                    id="ownership"
+                                                    name="ownership"
+                                                    value="TODO"
+                                                    className="form-control"
+                                                />
+                                                <label htmlFor="status" className="">Status</label>
+                                                <input
+                                                    type="text"
+                                                    id="status"
+                                                    name="status"
+                                                    value="TODO"
+                                                    className="form-control"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseDetails}>
+                Close
+            </Button>
+            <Button variant="primary" onClick={handleCloseDetails}>
+                Save Changes
+            </Button>
+        </Modal.Footer>
+    </Modal>
+    {/*  ============== EQUIPMENT DETAILS MODAL: END ============== */}
         </div>
     );
-
 }
 
 export default Events;
